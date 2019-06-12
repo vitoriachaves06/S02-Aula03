cc3850d4d1798eb67eedab9660ea0beceb1256bd
vitoria <chavesvitoria06@gmail.com>
2019-06-12 16:43:22 -0300

    criando lista for e funcao

---
+++ script.js
@@ -0,0 +1,60 @@
+// let materialEscada = prompt("digite o material da escada");
+// let degrau = materialEscada;
+// let qntd = Number(prompt("quantos degraus?")); 
+
+// for(let i=0; i<qntd; i++){
+//     console.log(materialEscada);
+//     materialEscada = materialEscada + degrau; 
+// }
+
+// let objeto={
+//     casa: [],
+//     carro: []
+// }
+
+// let filmes = [
+// {titulo: "Harry Potter", idade: 10},
+// {titulo: "It, a coisa", idade: 16},
+// {titulo: "O albergue", idade 18 anos},
+// ]
+
+// console.log(filmes[0].ator);
+
+// let filmes = [
+// {titulo: "Capita Marvel", idade: 12},
+// {titulo: "Detetive pikachu", idade 0},
+// {titulo: "Aladin", idade 10},
+// {titulo: "Vingadores:ultimato", idade: 16},
+// ]
+
+// let idade = prompt ("Digite sua idade");
+// for( let )
+
+
+// Função
+
+// let valor1 = Number (prompt("Digite o primeiro valor"));
+// let valor2 = Number (prompt("Digite o segundo valor "));
+
+//    function somar (n1,n2,n3){
+//     let media = (n1+n2+n3)/3; 
+//     console.log(`A media da carol é: ${media}`); 
+//    } 
+  
+// somar(5,8,10);  
+
+// calculadora = (a,b) =>{
+//     console.log (a+b)
+
+// Função anonima 
+// let fazerconta = function(a,b){
+//     console.log(a+b)
+// }
+
+function calcular (a,b,c){
+   let resultado = (a*,b/,c+);
+   console.log(
+
+  
+   }
+}
